6583ba2c0d0ba0ce669f1d3eb3e4a60dba92ab28
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-07-12 10:56:48 +0200

    added submit_all.sh script

---
+++ submit_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Submits alls script on euler. 
+
+# Accept additional parameters from the command line. This could eg be "--only_missing"
+additional_parameters=$@
+
+# Abort on first error found
+set -e
+
+for q in Q1 Q2 Q3 EK1 EK2 EK3; 
+do 
+    python learning_airfoils/python/submit_all_pure_python_in_parallel.py --functional_name ${q} --script python/learning_lax_sod.py --number_of_widths 1 --number_of_depths 1 ${additional_parameters}; 
+    python learning_airfoils/python/submit_all_pure_python_in_parallel.py --functional_name ${q} --script python/learning_lax_sod_mc.py --number_of_widths 1 --number_of_depths 1 ${additional_parameters}; 
+done ; 
+
