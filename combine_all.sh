4f964f344bc3cb96582951cda758f684c1c371b3
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-08-21 09:05:28 +0200

    added combine all script

--- combine_all.sh
+++ combine_all.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-for f in Q1 Q2 Q3 EK1 EK2 EK3; 
+for f in Q1 Q2 Q3;
 do 
-    bsub -W 120:00 -R 'rusage[mem=32000]' -N -B python learning_airfoils/python/combine_files.py /cluster/project/sam/klye/lax_sod_shock_tube_machine_learning/learning_lax_sod_$f ${f,,}  data/laxsod_$f;
+
+    if [[ $f = "Q"* ]]
+    then
+	fname=${f,,}
+    else
+	fname=${f}
+    fi
+
+    bsub -W 120:00 -R 'rusage[mem=32000]' -N -B python learning_airfoils/python/combine_files.py /cluster/project/sam/klye/lax_sod_shock_tube_machine_learning_early_stopping/learning_lax_sod_$f ${fname}  data/laxsod_$f;
 done
 
-for f in Q1 Q2 Q3 EK1 EK2 EK3; 
-do 
-    bsub -W 120:00 -R 'rusage[mem=32000]' -N -B python learning_airfoils/python/combine_files.py /cluster/project/sam/klye/lax_sod_shock_tube_machine_learning/learning_lax_sod_mc_$f ${f,,}  data/laxsod_mc_$f;
+for f in Q1 Q2 Q3;
+do
+    if [[ $f = "Q"* ]]
+    then
+	fname=${f,,}
+    else
+	fname=${f}
+    fi
+ 
+    bsub -W 120:00 -R 'rusage[mem=32000]' -N -B python learning_airfoils/python/combine_files.py /cluster/project/sam/klye/lax_sod_shock_tube_machine_learning_early_stoppingx/learning_lax_sod_mc_$f ${fname}  data/laxsod_mc_$f;
 done
